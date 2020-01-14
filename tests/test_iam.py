604d1969b32b343d7c4d5f6822dbbeda2bc7dac4
Johnatan Illi <johnatan.illi@exoscale.ch>
2020-01-14 14:53:27 +0100

    fix tests

--- tests/test_iam.py
+++ tests/test_iam.py
@@ -4,20 +4,19 @@
 import pytest
 from exoscale.api import ResourceNotFoundError
 from exoscale.api.iam import *
-import re
+from .conftest import _random_str
 
 class TestIAM:
     ### API key
 
-    def test_create_api_key(self, exo, test_prefix, operation):
+    def test_create_api_key(self, exo, test_prefix):
         api_key_name = "-".join([test_prefix, _random_str()])
-        api_key_operation = re.sub(r"\s+", "", s)
 
-        api_key = exo.iam.create_api_key(name=api_key_name, operations=operation)
+        api_key = exo.iam.create_api_key(name=api_key_name)
 
         assert api_key.name == api_key_name
         assert api_key.key != ""
-        assert ','.join(api_key.operation) != api_key_operation
+        assert api_key.operations != ""
         assert api_key.secret != ""
         assert api_key.type != ""
 
@@ -27,8 +26,7 @@ class TestIAM:
         api_key = APIKey._from_cs(exo.iam, apikey())
 
         api_keys = list(exo.iam.list_api_keys())
-        # We cannot guarantee that there will be only our resources in the
-        # testing environment, so we ensure we get at least our fixture Instance Pool
+     
         assert len(api_keys) >= 1
 
     def test_get_api_key(self, exo, apikey):
@@ -38,14 +36,13 @@ class TestIAM:
         assert _api_key.name == api_key.name
 
         with pytest.raises(ResourceNotFoundError) as excinfo:
-            _api_key = exo.compute.get_api_key(
+            _api_key = exo.iam.get_api_key(
                 key="EXO000000000000000000000000"
-                )
+            )
             assert _api_key is None
         assert excinfo.type == ResourceNotFoundError
 
     def test_list_api_key_operations(self, exo):
         operations = list(exo.iam.list_api_key_operations())
-        # We cannot guarantee that there will be only our resources in the
-        # testing environment, so we ensure we get at least our fixture Instance Pool
+       
         assert len(operations) >= 1
--- tests/test_iam_apikey.py
+++ tests/test_iam_apikey.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from exoscale.api.compute import *
+from exoscale.api.iam import *
+from .conftest import _random_str
 
 class TestIAMAPIKey:
     def test_revoke(self, exo, apikey):
-        api_key = APIKey._from_cs(exo.compute, apikey(teardown=False))
+        api_key = APIKey._from_cs(exo.iam, apikey(teardown=False))
         api_key_key = api_key.key
 
-        api_key.delete()
+        api_key.revoke()
         assert api_key.name is None
 
-        res = exo.compute.cs.listApiKeys(key=api_key_key, fetch_list=True)
+        res = exo.iam.cs.listApiKeys(key=api_key_key, fetch_list=True)
         assert len(res) == 0

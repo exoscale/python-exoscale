64a06462c6daafd20614012370a4e59afb610581
Marc Falzon <marc@exoscale.com>
2020-09-16 11:24:17 +0200

    Fix IAM revocation test

--- tests/test_iam_apikey.py
+++ tests/test_iam_apikey.py
@@ -8,10 +8,12 @@ from exoscale.api.iam import *
 class TestIAMAPIKey:
     def test_revoke(self, exo, apikey):
         api_key = APIKey._from_cs(exo.iam, apikey(teardown=False))
-        api_key_key = api_key.key
+        api_key_id = api_key.key
 
         api_key.revoke()
         assert api_key.name is None
 
-        res = exo.iam.cs.listApiKeys(key=api_key_key, fetch_list=True)
-        assert len(res) == 0
+        with pytest.raises(CloudStackApiException) as excinfo:
+            _api_key = exo.iam.cs.getApiKey(key=api_key_id)
+            assert _api_key is None
+        assert excinfo.type == CloudStackApiException

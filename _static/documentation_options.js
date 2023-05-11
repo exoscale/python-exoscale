3c84e211987b1ba036d62cac76bf88f6d3ee602d
brutasse <brutasse@users.noreply.github.com>
2023-05-11 15:33:55 +0000

    Deploying to gh-pages from @ exoscale/python-exoscale@54d9e090af7bd72d17bd73a7703a4c8557fd18e6 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2023-05-11',
+    VERSION: '0.8.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
